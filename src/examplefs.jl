49e1c8535f6d895a76fd84ab597215b618ebd94d
KlausC <klausC@users.noreply.github.com>
2021-12-22 11:52:32 +0100

    only one inode type

--- src/examplefs.jl
+++ src/examplefs.jl
@@ -9,6 +9,8 @@ import Base.Filesystem: S_IWUSR, S_IWGRP, S_IWOTH
 import Base.Filesystem: S_IXUSR, S_IXGRP, S_IXOTH
 import Base.Filesystem: S_ISUID, S_ISGID, S_ISVTX
 
+import Base: UV_ENOENT, UV_ENOTDIR
+
 const X_UGO = S_IRWXU | S_IRWXG | S_IRWXO
 const X_NOX = X_UGO & ~((S_IXUSR | S_IXGRP | S_IXOTH) & X_UGO)
 
@@ -27,52 +29,15 @@ mutable struct Inode
     ino::FuseIno
     mode::FuseMode
     nlink::Int64
+    size::UInt64
     atime::Timespec
     mtime::Timespec
     ctime::Timespec
 end
 
-abstract type Entity end
+is_directory(inode::Inode) = inode.mode & S_IFMT == S_IFDIR
+is_regular(inode::Inode) = inode.mode & S_IFMT == S_IFREG
 
-mutable struct File <: Entity
-    inode::Inode
-    size::Int64
-end
-
-struct Directory <: Entity
-    inode::Inode
-    direntries::Vector{Direntry}
-end
-
-Base.propertynames(e::E) where E<:Entity = (fieldnames(Inode)..., fieldnames(E)...)
-Base.getproperty(e::Entity, s::Symbol) = s in fieldnames(Inode) ? getfield(e.inode, s) : getfield(e, s)
-function Base.setproperty!(e::E, s::Symbol, v::Any) where E<:Entity
-    s in fieldnames(Inode) ?
-        setfield!(e.inode, s, convert(fieldtype(Inode, s), v)) :
-        setfield!(e, s, convert(fieldtype(E, s), v)) 
-end
-
-function Base.convert(::Type{Entity}, inode::Inode)
-    etm = inode.mode & S_IFMT
-    if etm == S_IFDIR
-        Directory(inode, Direntry[])
-    elseif etm == S_IFREG
-        File(inode, 0)
-    else
-        File(inode, 0)
-    end
-end 
-
-function status(e::File)
-    st = status(e.inode)
-    st.size = e.size
-    st
-end
-function status(e::Directory)
-    st = status(e.inode)
-    st.size = length(e.direntries)
-    st
-end
 
 function status(inode::Inode)
     st = CStructGuarded(Cstat)
@@ -84,6 +49,7 @@ function status(inode::Inode)
     st.atime = inode.atime
     st.mtime = inode.mtime
     st.ctime = inode.ctime
+    st.size = inode.size
     st
 end
 
@@ -91,15 +57,18 @@ function status(ino::FuseIno)
     status(get(INODES, ino, nothing))
 end
 
-const ROOT = Directory(Inode(FUSE_INO_ROOT, S_IFDIR | X_UGO, 1, Timespec(0,0), Timespec(0,0), Timespec(0,0)), Direntry[])
-const INODES = Dict{FuseIno, Entity}(FUSE_INO_ROOT => ROOT)
+const ROOT = Inode(FUSE_INO_ROOT, S_IFDIR | X_UGO, 1, 0, timespec_now(), timespec_now(), timespec_now())
+const INODES = Dict{FuseIno, Inode}(FUSE_INO_ROOT => ROOT)
 const DATA = Dict{FuseIno, Vector{UInt8}}()
+const DIR_DATA = Dict{FuseIno, Vector{Direntry}}()
 
 function example(parent, ino, name, data=nothing)
     t = timespec_now()
-    file = File(Inode(ino, S_IFREG | X_UGO, 1, t, t, t), 0)
+    file = Inode(ino, S_IFREG | X_UGO, 1, 0, t, t, t)
     push!(INODES, ino => file)
-    push!(INODES[parent].direntries, Direntry(ino, name))
+    direntries = get!(DIR_DATA, parent) do; Direntry[] end
+    push!(direntries, Direntry(ino, name))
+    INODES[parent].size = length(direntries)
     if data !== nothing
         d =  convert(Vector{UInt8}, data)
         file.size = length(d)
@@ -115,13 +84,14 @@ function lookup(req::FuseReq, parent::FuseIno, name::String)
 end
 
 function do_lookup(req::FuseReq, parent::FuseIno, name::String)
-    haskey(INODES, parent) || return Base.UV_ENOENT
+    haskey(INODES, parent) || return UV_ENOENT
     dir = INODES[parent]
-    dir isa Directory || return Base.UV_NODIR
-    k = findfirst(de -> de.name == name, dir.direntries)
-    k === nothing && return Base.UV_ENOENT
-    en = dir.direntries[k]
-    sta = status(en.ino)
+    is_directory(dir) || return UV_ENOTDIR
+    haskey(DIR_DATA, parent) || return UV_ENOENT
+    direntries = DIR_DATA[parent]
+    k = findfirst(de -> de.name == name, direntries)
+    k === nothing && return UV_ENOENT
+    sta = status(direntries[k].ino)
     entry = CStructGuarded(FuseEntryParam)
     entry.ino = sta.ino
     entry.attr_timeout = 0.0
@@ -137,7 +107,7 @@ end
 
 function getattr(req::FuseReq, ino::FuseIno, ::CStruct{FuseFileInfo})
     println("getattr(req, $ino, ...) called")
-    haskey(INODES, ino) || return Base.UV_ENOENT
+    haskey(INODES, ino) || return UV_ENOENT
     en = INODES[ino]
     attr = status(en)
     attr_timeout = 10.0
@@ -156,10 +126,10 @@ function readdir(req::FuseReq, ino::FuseIno, size::Integer, off::Integer, ::CStr
     println("readdir(req, $ino, $size, $off, fi) called")
     buf = Vector{UInt8}(undef, size)
     dir = INODES[ino]
-    dir isa Directory || return UV_ENODIR
+    is_directory(dir) || return UV_ENOTDIR
     i = off + 1
     p = 1
-    des = dir.direntries
+    des = get(DIR_DATA, ino) do; Direntry[] end
     n = length(des)
     rem = size
     while i <= n + 2
@@ -184,19 +154,22 @@ function readdir(req::FuseReq, ino::FuseIno, size::Integer, off::Integer, ::CStr
 end
 
 function create(req::FuseReq, parent::FuseIno, name::String, mode::FuseMode, fi::CStruct{FuseFileInfo})
+    dir = INODES[parent]
+    is_directory(dir) || return UV_ENOTDIR
     ino = maximum(keys(INODES)) + 1
     t = timespec_now()
-    inode = convert(Entity, Inode(ino, mode, 1, t, t, t))
+    inode = Inode(ino, mode, 1, 0, t, t, t)
     push!(INODES, ino => inode)
-    dir = INODES[parent]
-    push!(dir.direntries, Direntry(ino, name))
+    direntries = get!(DIR_DATA, parent) do; Direntry[] end
+    push!(direntries, Direntry(ino, name))
+    dir.size = length(direntries)
     entry = do_lookup(req, parent, name)
     fuse_reply_create(req, entry, fi)
 end
 
 function open(req::FuseReq, ino::FuseIno, fi::CStruct{FuseFileInfo})
     inode = INODES[ino]
-    if inode.mode & S_IFMT == S_IFREG && fi.flags & JL_O_TRUNC != 0
+    if is_regular(inode) && fi.flags & JL_O_TRUNC != 0
         inode.size = 0
         delete!(DATA, ino)
     end
@@ -205,8 +178,8 @@ end
 
 function read(req::FuseReq, ino::FuseIno, size::Integer, off::Integer, fi::CStruct{FuseFileInfo})
     save_fi(fi)
-    data = get(DATA, ino, UInt8[])
-    sz = min(length(data) - off, size)
+    data = get(DATA, ino) do; UInt8[] end
+    sz = max(min(length(data) - off, size), 0)
     bufv = CStructGuarded{FuseBufvec}(Cserialize(FuseBufvec, (count=1, buf=[(size=sz, mem = data)])))
     println("read(req, $ino, $size, $off) called, buffer = $bufv")
     if sz > 0
